b012afec22911f3b3bdf3197d4e490a3f19bc357
nichong99 <62277204+nichong99@users.noreply.github.com>
2020-04-14 10:34:03 +0800

    Update PatientEntityController.java

--- CARS/CARS-ejb/src/java/ejb/session/stateless/PatientEntityController.java
+++ CARS/CARS-ejb/src/java/ejb/session/stateless/PatientEntityController.java
@@ -13,6 +13,7 @@ import javax.persistence.NonUniqueResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import util.exception.PatientNotFoundException;
+import util.exception.InvalidLoginException;
 
 /**
  *
